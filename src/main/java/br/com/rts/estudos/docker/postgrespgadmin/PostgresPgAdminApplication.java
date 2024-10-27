07a431d26381705448e8a1facc8e69cb87bf80fa
Rodolpho T. Sotolani <rt.sotolani@gmail.com>
2024-10-27 12:15:25 -0400

    feat: Create a new Project with Docker compose support to Postgres and PgAdmin.

---
+++ src/main/java/br/com/rts/estudos/docker/postgrespgadmin/PostgresPgAdminApplication.java
@@ -0,0 +1,13 @@
+package br.com.rts.estudos.docker.postgrespgadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresPgAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostgresPgAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/rts/estudos/docker/postgrespgadmin/PostgresPgAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rts.estudos.docker.postgrespgadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresPgAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
